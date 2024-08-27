a3f3f07c15a8ec2013fecd2e9876a6158fa8c715
damithMasterNg <damithanushka95@gmail.com>
2024-08-27 23:55:15 +0530

    Add project main structure with dependencies

---
+++ src/main/java/com/example/taskmanagerpro/TaskmanagerproApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagerpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/taskmanagerpro/TaskmanagerproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagerpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
